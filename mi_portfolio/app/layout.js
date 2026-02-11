f184210a053006e0b342e64f5dd2c982e23fd208
Marcos Mellebovsky <marcosmellebovsky@MacBook-Pro-de-Marcos.local>
2026-02-11 17:30:38 -0300

    primer commit

---
+++ mi_portfolio/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Portfolio de Marcos Mellebovsky",
+  description: "Bienvenido a mi portfolio, donde comparto mis proyectos y habilidades en desarrollo web. Aquí encontrarás una selección de mis trabajos más destacados, desde aplicaciones web hasta sitios estáticos, demostrando mi pasión por la tecnología y el diseño. ¡Explora y descubre lo que puedo ofrecer!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
